c9aff2f0e1c669b9824479db7594666f8aef8a66
SUSocean <igonind.a1@gmail.com>
2023-03-28 12:54:51 +0300

    fixed imgae bug

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,7 +21,7 @@ function About() {
                     <i className="fa-brands fa-js"></i>
                     <i className="fa-brands fa-react"></i>
                     <i className="fa-solid fa-gears"></i>
-                    <img className="about-container--description--tools--nextjs" src="./public/images/next-js-icon.png" alt="nextJS logo" />
+                    <img className="about-container--description--tools--nextjs" src="../images/next-js-icon.png" alt="nextJS logo" />
                 </div>
             </div>
         </div>
